7caced86716ef46f762193b5edcd99c3d0c08b91
Crichmond21 <27980332+Crichmond21@users.noreply.github.com>
2020-06-25 21:16:16 -0400

    Rearanged and set background color

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function NavBar() {
+    return <NavBarContainer></NavBarContainer>;
+}
+
+const NavBarContainer = styled.div`
+    background: rgba(255, 255, 255, .16);
+    height: 100px;
+    width: 100%;
+`;
