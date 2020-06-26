a909bb8d043736a97cc465cf48bf4368e68fd660
Crichmond21 <27980332+Crichmond21@users.noreply.github.com>
2020-06-25 22:38:20 -0400

    Updated formatting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { NavBar } from './components/NavBar';
+import React from "react";
+import { NavBar } from "./components/NavBar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,76 +1,61 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useHistory } from 'react-router';
-import { ProfileIcon } from '../icons/ProfileIcon';
+import React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router";
+import { ProfileIcon } from "../icons/ProfileIcon";
 
 export function NavBar() {
-    const history = useHistory()
-    return (
-        <NavBarContainer>
-            <LogoContainer>Project Capitalizer</LogoContainer>
-            <LinkContainer>
-                <Link
-                    path={'/dashboard'}
-                    history={history}
-                >
-                    Dashboard
-                </Link>
+  const history = useHistory();
+  return (
+    <NavBarContainer>
+      <LogoContainer>Project Capitalizer</LogoContainer>
+      <LinkContainer>
+        <Link path={"/dashboard"} history={history}>
+          Dashboard
+        </Link>
 
-                <Link
-                    path={'/stocks'}
-                    history={history}
-                >
-                    Stocks
-                </Link>
+        <Link path={"/stocks"} history={history}>
+          Stocks
+        </Link>
 
-                <Link
-                    path={'/profile'}
-                    history={history}
-                >
-                    <ProfileIcon></ProfileIcon>
-                </Link>
-            </LinkContainer>
-        </NavBarContainer>
-    );
+        <Link path={"/profile"} history={history}>
+          <ProfileIcon></ProfileIcon>
+        </Link>
+      </LinkContainer>
+    </NavBarContainer>
+  );
 }
 
 const NavBarContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background: rgba(255, 255, 255, .16);
-    height: 100px;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: rgba(255, 255, 255, 0.16);
+  height: 100px;
+  width: 100%;
 `;
 
 const LogoContainer = styled.div`
-    padding-left: 30px;
-    color: white;
-    font-size: 45px
+  padding-left: 30px;
+  color: white;
+  font-size: 45px;
 `;
 
 const LinkContainer = styled.div`
-    display: flex;
-    align-items: center;
-    padding-right: 30px;
-`
-
-const LinkWrapper = styled.div`
-    color: white;
-    font-size: 25px;
-    padding: 30px
+  display: flex;
+  align-items: center;
+  padding-right: 30px;
 `;
 
-const Link = ({path, history, children}) => {
-    let handleClick = (() => {
-        history.push(path);
-    })
+const LinkWrapper = styled.div`
+  color: white;
+  font-size: 25px;
+  padding: 30px;
+`;
 
-    return (
-        <LinkWrapper
-            onClick={handleClick}
-        >
-            {children}
-        </LinkWrapper>
-    );
+const Link = ({ path, history, children }) => {
+  let handleClick = () => {
+    history.push(path);
+  };
+
+  return <LinkWrapper onClick={handleClick}>{children}</LinkWrapper>;
 };
