9a3925d3723118b550dfd791d8d7bd0fab8965d2
Crichmond21 <27980332+Crichmond21@users.noreply.github.com>
2020-06-28 20:09:54 -0400

    Implemented

---
+++ src/Context.js
@@ -0,0 +1,27 @@
+import React, { useReducer, createContext } from "react";
+
+const initialData = {
+  searchField: "",
+};
+
+export const CapitalizerContext = createContext([initialData, () => {}]);
+
+function capitalizerReducer(state, action) {
+  switch (action.type) {
+    case "update search": {
+      return { ...state, searchField: action.searchField };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+export function CapitalizerProvider({ children }) {
+  const [state, dispatch] = useReducer(capitalizerReducer, initialData);
+  return (
+    <CapitalizerContext.Provider value={[state, dispatch]}>
+      {children}
+    </CapitalizerContext.Provider>
+  );
+}
