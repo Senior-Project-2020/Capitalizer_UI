88d8c0c7f336789badb18872374a3bc90fcf383c
Crichmond21 <27980332+Crichmond21@users.noreply.github.com>
2020-07-03 10:04:59 -0400

    Formatted

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -3,14 +3,16 @@ import { InterestSelector } from "../components/InterestSelector";
 import { LogOutButton } from "../components/Buttons";
 import { Key } from "../components/Key";
 
-export function ProfilePage(){
-    const testKey = "FD#&@Sjfs812j21jlf"
-    return (
-        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-            <h1 style={{color: "white"}}>Welcome Carter!</h1>
-            <Key apikey={testKey}></Key>
-            <InterestSelector></InterestSelector>
-            <LogOutButton></LogOutButton>
-        </div> 
-    );
+export function ProfilePage() {
+  const testKey = "FD#&@Sjfs812j21jlf";
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1 style={{ color: "white" }}>Welcome Carter!</h1>
+      <Key apikey={testKey}></Key>
+      <InterestSelector></InterestSelector>
+      <LogOutButton></LogOutButton>
+    </div>
+  );
 }
