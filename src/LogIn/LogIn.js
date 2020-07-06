1fbf3a79c645bd681348a787251314542c4dea4d
Crichmond21 <27980332+Crichmond21@users.noreply.github.com>
2020-07-05 21:23:23 -0400

    Added login button and framework for sending api request to validate

--- src/LogIn/LogIn.js
+++ src/LogIn/LogIn.js
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { CredentialsForm } from "../components/CredentialsForm";
+import { LogInButton } from "../components/Buttons";
+import { CapitalizerContext } from "../Context";
+import axios from "axios";
 
 export function LogInPage() {
+  const [context, updateContext] = useContext(CapitalizerContext);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  function handleClick(event){
+    //Send auth request here then update the context with the returned user
+  }
+
   return (
-    <CredentialsForm
-      username={username}
-      setUsername={setUsername}
-      password={password}
-      setPassword={setPassword}
-    ></CredentialsForm>
-    
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "200px"}}>
+      <CredentialsForm
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      ></CredentialsForm>
+      <LogInButton handleClick={handleClick}></LogInButton>
+    </div>
   );
 }
